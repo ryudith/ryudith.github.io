c030ff017a830fa0a2c959cad7416d11c2b69527
ryudith <ryudith_anggriawan@yahoo.co.id>
2018-05-03 13:59:00 +0700

    pitpoint 201805031358 - all basic usage service worker and notification.

--- sw.js
+++ sw.js
@@ -1,43 +1,7 @@
-function handleNotification (title, message, img) {
-	var opts = {
-		body: message,
-		badge: '/hexa.png',
-		icon: '/hexa.png',
-		image: img || '/hexa.png',
-		vibrate: [200, 100, 200, 100, 200, 100, 200],
-		tag: 'test-sample',
-		renotify: true,
-
-		// code below must using serviceWorker
-		actions: [  
-			{action: 'like', title: 'Like'},
-			{action: 'reply', title: 'Reply'}
-		]
-	};
-
-	if (! ('Notification' in window)) {
-		alert('Your Browser Not Support Notification !');
-	} else if (Notification.permission === 'granted') {
-		navigator.serviceWorker.ready.then(function(registration) {
-			registration.showNotification(title, opts);
-			// setTimeout(registration.close.bind(registration), 2000);
-		});
-
-	} else if (Notification.permission !== 'denied') {
-		Notification.requestPermission(function (permission) {
-			navigator.serviceWorker.ready.then(function(registration) {
-				registration.showNotification(title, opts);
-				// setTimeout(registration.close.bind(registration), 2000);
-			});
-		});
-
-	}
-}
-
-
 // The SW will be shutdown when not in use to save memory,
 // be aware that any global state is likely to disappear
 console.log("SW startup");
+// console.log(self.navigator.serviceWorker.ready.then);
 
 self.addEventListener('install', function (event) {
 	var cacheName = 'test-pwa-cache';
@@ -45,18 +9,15 @@ self.addEventListener('install', function (event) {
 
 	event.waitUntil(caches.open(cacheName).then(function (cache) {
 		console.log('opened cache');
-		handleNotification('Info', 'PWA Opened Cache (Install).');
 		return cache.addAll(urlCache);
 	}));
 });
 
 self.addEventListener('activate', function(event) {
 	console.log("SW activated");
-	handleNotification('Info', 'PWA Active.');
 });
 
 self.addEventListener('fetch', function(event) {
 	console.log("Caught a fetch!");
-	handleNotification('Info', 'PWA Fetch.');
 	event.respondWith(new Response("Hello world!"));
 });
